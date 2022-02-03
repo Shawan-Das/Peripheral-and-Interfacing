d1324122484f033c099251cd546ff2990b133cad
Shawan Das <shawondas7429@gmail.com>
2022-02-03 14:19:15 +0600

    Analog Time Counter/final update

---
+++ Project_Folder/Project_Code-02/Project_Code-02.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(2,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(13,HIGH);
+  digitalWrite(10,HIGH);
+}
