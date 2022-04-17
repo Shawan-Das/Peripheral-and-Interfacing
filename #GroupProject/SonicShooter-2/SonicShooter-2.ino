a4ee6c9dc81a1f83e0d4806222998bb2fd387fd0
Shawan Das <shawondas7429@gmail.com>
2022-04-17 18:05:20 +0600

    Final Update: SonicShooter-2/Final_Code

--- #GroupProject/SonicShooter-2/SonicShooter-2.ino
+++ #GroupProject/SonicShooter-2/SonicShooter-2.ino
@@ -1,23 +1,21 @@
 #include<Wire.h>
 #include<LiquidCrystal_I2C.h>
 #include<Servo.h>
-#include<Servo.h>
+
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
-Servo motor1;
-// defines variables
+Servo myServo1, myServo2;
+
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
 byte trigger ;
-Servo myServo1, myServo2;
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void servo(){
-  for(int i=0; i<=180; i++){
-    myServo1.write(i);
-    myServo2.write(i);
-  }
-}
+void servo();
+int sonic();
+void hit(int distance);
+void miss(int distance);
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
@@ -26,61 +24,79 @@ void setup() {
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
   lcd.init();
   lcd.backlight();
+  myServo1.attach(12);
 
-  motor1.attach(31);
 }
 
 
 void loop() {
-  // Clears the trigPin condition
   trigger = digitalRead(8);
-  //Serial.println(trigger);
-//
-//  for(int i=10; i<=1000; i+=10){
-//      tone(7,i,50); delay(500);
-//    }
-//    
+ 
   lcd.clear();
   lcd.print("Shoot The TARGET");
-delay(500); lcd.clear();
- for (int i=0; i<=180; i+=10) motor1.write(i), delay(30); 
-    if(trigger == 0)
+  
+  if(trigger == 0)
   {
     lcd.clear();
     tone(7,50,100);
+    delay(100);   
     
-    digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
+    distance = sonic(); 
 
-  lcd.print(distance);
-   //servo(); 
-
-     if(distance>=80  && distance<= 150){
-      
-      tone(7,5000,100);
-      delay(100);
-      tone(7,5000,200);
-        lcd.clear();
-        lcd.print("Hit"); delay(999);
-        lcd.setCursor(0,1); lcd.print(distance); delay(999); 
-     }
-     else{
-      tone(7,1500,100);
-      lcd.clear();
-      lcd.print("MISS"); delay(600);
-      lcd.setCursor(0,1); lcd.print(distance); delay(999); 
-     }
+    if(distance>=90  && distance<= 150)
+      hit(distance);
+    else
+      miss(distance);   
   
   }
-  //trigger = 3;
+
   delay(100);
+}
+
+void servo(){
+  for(int i=0; i<=180; i++){
+    myServo1.write(i);
+    myServo2.write(i);
+    delay(5);
+  }
+  for(int i=180; i>0; i--){
+    myServo1.write(i);
+    myServo2.write(i);
+    delay(5);
+  }
+}
+
+int sonic()
+{
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+  
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);// Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(trigPin, LOW);
+     
+    duration = pulseIn(echoPin, HIGH);// Reads the echoPin, returns the sound wave travel time in microseconds
+
+    return duration * 0.034 / 2;// Speed of sound wave divided by 2 (go and back)// Calculating the distance
+}
+
+void hit(int distance)
+{
+  
+      tone(7,5000,100);
+      servo();
+      delay(100);
+      tone(7,5000,200);
+      lcd.clear();
+      lcd.print("Hit"); delay(999);
+      lcd.setCursor(0,1); lcd.print(distance); delay(999); 
+      
+}
+
+void miss(int distance)
+{
+  tone(7,200,100);
+  lcd.clear();
+  lcd.print("MISS"); delay(600);
+  lcd.setCursor(0,1); lcd.print(distance); delay(999);
 }
